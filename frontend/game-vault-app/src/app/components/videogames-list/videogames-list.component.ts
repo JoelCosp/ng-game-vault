c31a5aaa22cd787505a2391afd0b1a77210d763a
JoelVRV <171981845+JoelCosp@users.noreply.github.com>
2025-04-01 01:14:30 +0200

    FILTROS POR PRECIO Y POR NOMBRE

--- frontend/game-vault-app/src/app/components/videogames-list/videogames-list.component.ts
+++ frontend/game-vault-app/src/app/components/videogames-list/videogames-list.component.ts
@@ -3,25 +3,51 @@ import { VideogamesService } from '../../services/videogames.service';
 import { Videogame } from '../../models/Videogame';
 import { CommonModule } from '@angular/common';
 import { VideogamesListCardComponent } from '../videogames-list-card/videogames-list-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-videogames-list',
-  imports: [CommonModule, VideogamesListCardComponent],
+  imports: [CommonModule, FormsModule, VideogamesListCardComponent],
   templateUrl: './videogames-list.component.html',
   styleUrl: './videogames-list.component.css'
 })
 export class VideogamesListComponent implements OnInit{
   
   videogames: Videogame[];
+  filteredVideogames: Videogame[];
+  priceFilter!: string;
+  searchText!: string;
 
   constructor(public videogamesService: VideogamesService) {
     this.videogames = [];
+    this.filteredVideogames = [];
+    this.priceFilter = '';
+    this.searchText = '';
   }
 
   ngOnInit(): void {
     this.videogamesService.getVideogames().subscribe((videogame) => {
       this.videogames = videogame;
-      console.log(this.videogames);
+      this.filteredVideogames = videogame;
     })
+  }
+
+  applyFilters(): void {
+    this.filteredVideogames = this.videogames.filter((videogame) => {
+      let isValid = true;
+      
+      // Filtro por precio
+      if (this.priceFilter === 'low') {
+        isValid = isValid && Number(videogame.price) <= 30;
+      } else if (this.priceFilter === 'high') {
+        isValid = isValid && Number(videogame.price) > 30;
+      }
+
+      if (this.searchText) {
+        isValid = isValid && videogame.title.toLowerCase().includes(this.searchText.toLowerCase());
+      }
+
+      return isValid;
+    });
   }
 }
